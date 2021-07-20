d8444f1f0c96752aefd2983512b39a00225ae0a5
StepanSwhite <stepan.swhite@gmail.com>
2021-07-20 14:14:08 +0300

    add: smooth scroll on sections

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,50 @@
-let intro = $('#intro');
-let header = $('#header');
-let introHeight = intro.innerHeight();
-let headerHeight = header.innerHeight();
-$(window).on('scroll', function() {
+$(function() {
 
-    let scrollTop = $(this).scrollTop();
+   let intro = $("#intro");
+   let header = $("#header");
+   let introH = intro.innerHeight();
+   let headerH = header.innerHeight();
 
-    if( scrollTop >= (introHeight - headerHeight) ) {
-        header.addClass('header--dark');
-     } else {
-        header.removeClass('header--dark');
-     }
+
+   /* Header class on scroll
+   =====================================*/
+
+   headerScroll();
+
+   $(window).on("scroll  resize", function() {
+       headerScroll();
+   });
+
+   function headerScroll() {
+       introH = intro.innerHeight();
+       headerH = header.innerHeight();
+
+       let scrollTop = $(this).scrollTop();
+
+       if( scrollTop >= (introH - headerH) ) {
+           header.addClass("header--dark");
+       } else {
+           header.removeClass("header--dark");
+       }
+   }
+
+
+
+   /* Smooth Scroll to sections
+   =====================================*/
+
+   $("[data-scroll]").on("click", function(event) {
+       event.preventDefault();
+
+       let scrollEl = $(this).data("scroll");
+       let scrollElPos = $(scrollEl).offset().top;
+
+       $("html, body").animate({
+           scrollTop: scrollElPos - headerH
+       }, 500)
+   });
+
+
+
 
 });
-
-console.log();
